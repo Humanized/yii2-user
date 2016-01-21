da273e86fed9e4b2d8cc819f69950ec8d5dd68b4
jeffrey.geyssens <jeffrey@humanized.be>
2016-01-21 23:32:46 +0100

    Module Option Documentation

--- Module.php
+++ Module.php
@@ -3,33 +3,35 @@
 namespace humanized\user;
 
 /**
- * User Administration Module for Yii2 - By Humanized
- * This module wraps and extends the default user management interface provided by the framework. 
- *
- * It is designed to be fully compatible with the factory-provided implementations related to user management.
- * For this, several configuration options can be set to override module defaults allowing the use of the factory default or custom classes.
+ * User Administration Module for Yii2
  * 
+ * This module wraps and extends the basic user management features, provided by the stock advanced Yii2 framework template.
  * 
+ * Though implementations vary, the code interface as provided by the advanced template has been ported in it's entirety.
  * 
+ * This allows using the module (with it's default settings)  to be used as a drop-in replacement for default user managment,
+ * without having to worry about breaking code existing code (other than namespace renaming).   
  * 
- * Provides several interfaces for dealing with Yii2 based user accounts:
+ *  
  * 
  * GUI
  * 
- * This module wraps the default user-management provided by the system
+ * Default user-management  provided by the system
  * 
- * 
- * REST API
- * 
- * Under Construction - Due v0.5
  * 
  * CLI 
  * 
  * A CLI allowing basic Yii2 user administration functions.
  * 
  * 
- * @name Yii2 User Administration Module CLass 
- * @version 0.0.1 
+ * REST API
+ * 
+ * Under Construction - Due v0.5
+ * 
+ * 
+ * 
+ * @name Yii2 User Administration Module Class 
+ * @version 0.1 
  * @author Jeffrey Geyssens <jeffrey@humanized.be>
  * @package yii2-user
  */
@@ -37,15 +39,103 @@ class Module extends \yii\base\Module {
 
     public $identityClass = NULL;
     public $fnUser = NULL;
-    public $emailOnly = false;
-    public $enableSignUp = true;
-    public $enablePasswords = true;
-    public $enablePjax = false;
-    public $enableRBAC = false;
-    public $enableKartik = false;
-    public $enableDynaGrid = false;
-    //User Status Code
-    public $enableStatusCodes = true;
+
+    /**
+     * @since 0.1
+     * @var boolean Enable account username attribute. 
+     * 
+     * When disabled, account email address is used for identification purposes.
+     * 
+     * Defaults to TRUE
+     * 
+     * 
+     */
+    public $enableUserName = TRUE;
+
+    /**
+     * @since 0.1
+     * @var boolean Enable access token authentication. 
+     * 
+     * When enabled, authentication tokens can be generated used to authenticate accounts over passwords
+     * 
+     * Defaults to FALSE
+     * 
+     */
+    public $enableTokenAuthentication = FALSE;
+
+    /**
+     * @since 0.1
+     * @var boolean Enable public account creation. 
+     * 
+     * When enabled, the GUI allows visitors to sign-up for an account on the system
+     * 
+     * Defaults to TRUE
+     * 
+     */
+    public $enableSignUp = TRUE;
+
+    /**
+     * @todo Implement this option
+     * @var boolean Enable verification of public account creation. 
+     * 
+     * When enabled, account creation must be confirmed, by clicking the confirmation link sent to the account e-mail address.
+     * 
+     * Defaults to FALSE
+     * 
+     */
+    public $enableUserVerification = FALSE;
+
+    /**
+     * @todo Implement this option
+     * @var boolean Enable verification of public account creation by system administrators. 
+     * 
+     * When enabled, account creation must be confirmed through the user management facilities provided by the module
+     * 
+     * Defaults to FALSE
+     * 
+     */
+    public $enableAdminVerification = FALSE;
+
+    /**
+     * @todo Implement this feature
+     * @var boolean Enable password-based login. 
+     * 
+     * When enabled, users are required to provide a password when logging in to the system 
+     * 
+     * Defaults to TRUE
+     * 
+     */
+    public $enablePasswords = TRUE;
+
+    /**
+     * @todo Implement this option
+     * @var boolean Enable PJAX on GUI
+     * 
+     * When enabled, PJAX is used to provide various usability enchancements.
+     * 
+     * Defaults to FALSE 
+     */
+    public $enablePjax = FALSE;
+
+    /**
+     * @since 0.1
+     * @var boolean Enable role cased authorisation control
+     * 
+     * When enabled, several mechanisms are provided to incorporate user accounts with the default RBAC interface provided by the framework.
+     * 
+     * Defaults to FALSE    
+     */
+    public $enableRBAC = FALSE;
+
+    /**
+     * @since 0.1
+     * @var boolean Enable account status mechanism 
+     * 
+     * When enabled, several mechanisms are provided to incorporate user accounts with the default RBAC interface provided by the framework.
+     * 
+     * Defaults to FALSE    
+     */
+    public $enableStatusCodes = FALSE;
     public $statusCodeTable = NULL;
     public $statusCodeTableId = 'id';
     public $statusCodeTableName = 'name';
