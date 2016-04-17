63bbb2e6f6176064548f1e00658b6b03caf14c01
jeffrey <jeffrey@localhost.localdomain>
2016-04-17 20:17:17 +0200

    [MOD] Fixing password reset

--- models/common/ResetPasswordForm.php
+++ models/common/ResetPasswordForm.php
@@ -71,7 +71,7 @@ class ResetPasswordForm extends Model
         $user = $this->_user;
         $user->setPassword($this->password);
         $user->generatePassword = FALSE;
-        $user->removePasswordResetToken();
+        //$user->removePasswordResetToken();
 
         return $user->save(false);
     }
